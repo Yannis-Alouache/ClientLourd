c868a68bb907df2af608120a17b410ea202e0a18
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-03-27 17:18:05 +0200

    [+] added model

--- src/main/java/org/tp/Db.java
+++ src/main/java/org/tp/Db.java
@@ -1,5 +1,6 @@
 package org.tp;
 import java.sql.*;
+import java.util.ArrayList;
 
 import static java.lang.System.exit;
 
@@ -8,7 +9,7 @@ public class Db {
     private Connection conn;
     public Db(String host, String port, String dbName, String user, String password) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             this.conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + dbName, user, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -20,13 +21,32 @@ public class Db {
 
     }
 
-    public ResultSet getTravelerBoatsWithEquipements() {
-        Statement stmt = null;
+    public ResultSet getBoatByName(String name) {
+        ArrayList<String> array = new ArrayList<String>();
+        String sql = "Select * from bateau WHERE bateau.nom = ?";
+        PreparedStatement stmt = null;
+        ResultSet res = null;
 
         try {
-            stmt = conn.createStatement();
-            String sql = "SELECT bateau.*, GROUP_CONCAT(equipement.libelle SEPARATOR ', ') AS equipements FROM voyageur_equipement, voyageur, bateau, equipement WHERE voyageur_equipement.id_voyageur = voyageur.codeBateau AND voyageur.codeBateau = bateau.codeBateau and equipement.id = voyageur_equipement.id_equipement GROUP BY bateau.nom";
-            ResultSet res = stmt.executeQuery(sql);
+            stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, name);
+            res = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return res;
+    }
+
+    public ResultSet getTravelerBoatsWithEquipements(String name) {
+        PreparedStatement stmt = null;
+
+        try {
+            String sql = "SELECT bateau.*, voyageur.imageLink, GROUP_CONCAT(equipements.id, \",\", equipements.libelle SEPARATOR ':') AS equipements FROM equipements_voyageur, voyageur, bateau, equipements WHERE equipements_voyageur.id_voyageur = voyageur.codeBateau AND voyageur.codeBateau = bateau.codeBateau and equipements.id = equipements_voyageur.id_equipement and bateau.nom = ? GROUP BY bateau.nom";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, name);
+
+            ResultSet res = stmt.executeQuery();
 
             return res;
 
@@ -35,6 +55,7 @@ public class Db {
         }
         return null;
     }
+
 
     public ResultSet getBoatsName() {
         Statement stmt = null;
